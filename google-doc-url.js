c26707131d6fee77d3284f6b593f2e0da53ab4c8
irKoen <35067109+irKoen@users.noreply.github.com>
2018-01-13 11:29:44 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQea1dUCqbBo9nMJJbKsHUrQqIe02JF5TXge0eBgIVkQbRdos5q4diBmw0M_h0xwWA4jj5XQ8s8cL3Y/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/126FixkWsxVA73db_m-TvdwBcW5org8a5FPdtcWMJGJI/edit#gid=0';
